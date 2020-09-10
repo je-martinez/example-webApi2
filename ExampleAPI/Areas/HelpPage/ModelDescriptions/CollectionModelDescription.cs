2b2be3d78d1e0bf5df8b1fdee22886f7f27905e2
Jose Martinez <jose.martinez@cit.hn>
2020-09-10 11:34:13 -0600

    +Inicio de Proyecto.

---
+++ ExampleAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ExampleAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
